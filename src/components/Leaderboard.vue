2351eee22a07f8780bc96e396d94b6c333e48d18
onurtaskinn <onurtaskin180@gmail.com>
2023-05-08 06:58:21 +0200

    leaderboard updated

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,141 +1,124 @@
-   <template>
-    <div class="container">
-      <h2>Leaderboard</h2>
-      <div class="podium">
-        <div
-          v-for="(participant, index) in participants.slice(0, 3)"
-          :key="participant.id"
-          class="podium-column"
-          :class="`podium-${index + 1}`"
-          :style="{
-            height: ((participant.points / maxPoints) * 100 || 0) + '%',
-          }"
-        >
+<template>
+  <div class="container">
+    <h2>Leaderboard</h2>
+    <div class="podium">
+      <div
+        v-for="(participant, index) in participants.slice(0, 3)"
+        :key="participant.id"
+        class="podium-column"
+        :class="`podium-${index + 1}`"
+        :style="{
+          height: ((participant.points / maxPoints) * 100 || 0) + '%',
+          backgroundColor: podiumColors[index],
+        }"
+      >
+        <div class="podium-text">
+          <div class="podium-rank">{{ index + 1 }}</div>
           <div class="podium-name">{{ participant.alias }}</div>
           <div class="podium-points">{{ participant.points }}</div>
         </div>
       </div>
-      <div class="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Alias</th>
-              <th>Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(participant, index) in participants" :key="participant.id">
-              <td>{{ index + 1 }}</td>
-              <td>{{ participant.alias }}</td>
-              <td>{{ participant.points }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
     </div>
-  </template>
-  
-  <style scoped>
-  .container {
-    font-family: Arial, sans-serif;
-    max-width: 800px;
-    margin: 0 auto;
-  }
-  
-  h2 {
-    text-align: center;
-    margin-bottom: 1rem;
-    color: black;
-  }
-  
-  .podium {
-    display: flex;
-    justify-content: center;
-    align-items: flex-end;
-    margin-bottom: 1rem;
-    height: 250px;
-  }
-  
-  .podium-column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
-    width: 20%;
-    margin: 0 1rem;
-    background-color: #f2f2f2;
-    border-radius: 5px;
-    position: relative;
-    transition: height 0.5s;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    padding-bottom: 1.5rem; /* Add this line */
-  }
+    <div class="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Alias</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(participant, index) in participants" :key="participant.id">
+            <td>{{ index + 1 }}</td>
+            <td>{{ participant.alias }}</td>
+            <td>{{ participant.points }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
 
-  .podium-text {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  text-align: center;
-  background-color: white;
-  padding: 5px 0;
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
 }
-  
-  .podium-name {
-    margin-top: 0.5rem;
-    padding: 0 5px; /* Add this line */
-    color: black;
-  }
-  
-  .podium-points {
-    margin-top: 0.25rem;
-    font-weight: bold;
-    padding: 0 5px; /* Add this line */
-    color: black;
-  }
-  
-  .podium-1 {
-    background-color: #F6BC44;
-  }
-  
-  .podium-2 {
-    background-color: #E5E5E5;
-  }
-  
-  .podium-3 {
-    background-color: #B57B33;
-  }
-  
-  .table-container {
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    overflow: hidden;
-    margin-bottom: 100px; /* Add this line */
-  }
-  
-  table {
-    width: 100%;
-    border-collapse: collapse;
-    color: black;
-  }
-  
-  thead {
-    background-color: #f2f2f2;
-  }
-  
-  th,
-  td {
-    text-align: left;
-    padding: 0.5rem;
-  }
-  
-  tr:nth-child(even) {
-    background-color: #f7f7f7;
-  }
-  
-  </style>
-  
+.container h2{
+  color: rgb(0, 43, 172);
+  text-align: center;
+}
+
+.podium {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-bottom: 1rem;
+  padding: 0 1rem;
+}
+
+.podium-column {
+  flex: 1;
+  margin: 0 0.5rem;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  text-align: center;
+  border-radius: 8px;
+}
+
+.podium-rank {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.podium-name {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.podium-points {
+  font-size: 1rem;
+  font-weight: bold;
+}
+
+.podium-text {
+  color: rgb(0, 0, 203);
+}
+
+.table-container {
+  overflow-x: auto;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  padding: 0.5rem;
+  text-align: left;
+  color: black;
+}
+
+th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+
+tr:nth-child(odd) {
+  background-color: #f9f9f9;
+}
+</style>
+
+
+
 
 
 
@@ -178,3 +161,4 @@
     },
   };
   </script>
+  
